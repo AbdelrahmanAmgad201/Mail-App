8c9e01bfe8a6c0a6d83326b3645430cec4077156
mahmoud842 <mahmoud1hesham2003@gmail.com>
2024-12-09 00:31:13 +0200

    first

---
+++ mail-frontend/src/inbox/Inbox.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './Inbox.css'
+
+function Inbox(props) {
+
+  return (
+    <div className='main'>
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Inbox
