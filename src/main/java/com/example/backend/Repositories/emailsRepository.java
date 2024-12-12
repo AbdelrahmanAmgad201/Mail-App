3a70fe6e1395a6dda7779c16f6d25cd832f25c98
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2024-12-12 11:01:08 +0200

    fixing

--- src/main/java/com/example/backend/Repositories/emailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repositories;
-
-import com.example.backend.Entities.emails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface emailsRepository extends JpaRepository<emails, Long> {
-
-}
